e6997a0d0a178e627f760e721d9a6f54848ef7d5
ish <tmdghks0615@gmail.com>
2025-04-16 22:38:15 +0900

    공통코드 api 엔드포인트 변경

--- src/main/java/io/madeformaid/common/commonCode/controller/CommonCodeController.java
+++ src/main/java/io/madeformaid/common/commonCode/controller/CommonCodeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/common-code")
+@RequestMapping("/api/common/code")
 @RequiredArgsConstructor
 public class CommonCodeController {
     private final CommonCodeService commonCodeService;
