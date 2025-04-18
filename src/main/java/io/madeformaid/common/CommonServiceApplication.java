c40cfb25dec5d1fc76c0031b3eac51471e8ac01a
ish <tmdghks0615@gmail.com>
2025-04-18 18:30:26 +0900

    GlobalExceptionHandler 추가

--- src/main/java/io/madeformaid/common/CommonServiceApplication.java
+++ src/main/java/io/madeformaid/common/CommonServiceApplication.java
@@ -1,12 +1,14 @@
 package io.madeformaid.common;
 
 import io.madeformaid.shared.context.EnableAuthContext;
+import io.madeformaid.shared.exception.EnableGlobalExceptionHandling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @EnableAuthContext
+@EnableGlobalExceptionHandling
 @SpringBootApplication
 public class CommonServiceApplication {
 
